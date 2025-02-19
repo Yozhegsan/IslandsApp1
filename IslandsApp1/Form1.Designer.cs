6f1ee67179d12b7f89663be6ff40ace87365e1a3
Олександр Гнідін <yozhegsan@gmail.com>
2025-02-19 11:32:20 +0200

    Next

--- IslandsApp1/Form1.Designer.cs
+++ IslandsApp1/Form1.Designer.cs
@@ -28,79 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.pic = new System.Windows.Forms.PictureBox();
-            this.lstMap = new System.Windows.Forms.ListBox();
-            this.lstIsland = new System.Windows.Forms.ListBox();
-            this.picFlames = new System.Windows.Forms.PictureBox();
-            this.tmrFlames = new System.Windows.Forms.Timer(this.components);
-            this.tmrMain = new System.Windows.Forms.Timer(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picFlames)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // pic
-            // 
-            this.pic.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pic.Location = new System.Drawing.Point(93, 12);
-            this.pic.Name = "pic";
-            this.pic.Size = new System.Drawing.Size(587, 534);
-            this.pic.TabIndex = 0;
-            this.pic.TabStop = false;
-            // 
-            // lstMap
-            // 
-            this.lstMap.Enabled = false;
-            this.lstMap.FormattingEnabled = true;
-            this.lstMap.Location = new System.Drawing.Point(12, 12);
-            this.lstMap.Name = "lstMap";
-            this.lstMap.Size = new System.Drawing.Size(75, 69);
-            this.lstMap.TabIndex = 1;
-            this.lstMap.SelectedIndexChanged += new System.EventHandler(this.lstMap_SelectedIndexChanged);
-            // 
-            // lstIsland
-            // 
-            this.lstIsland.Enabled = false;
-            this.lstIsland.FormattingEnabled = true;
-            this.lstIsland.Location = new System.Drawing.Point(12, 87);
-            this.lstIsland.Name = "lstIsland";
-            this.lstIsland.Size = new System.Drawing.Size(75, 69);
-            this.lstIsland.TabIndex = 2;
-            this.lstIsland.SelectedIndexChanged += new System.EventHandler(this.lstIsland_SelectedIndexChanged);
-            // 
-            // picFlames
-            // 
-            this.picFlames.Location = new System.Drawing.Point(12, 440);
-            this.picFlames.Name = "picFlames";
-            this.picFlames.Size = new System.Drawing.Size(34, 50);
-            this.picFlames.TabIndex = 3;
-            this.picFlames.TabStop = false;
-            // 
-            // tmrFlames
-            // 
-            this.tmrFlames.Tick += new System.EventHandler(this.tmrFlames_Tick);
-            // 
-            // tmrMain
-            // 
-            this.tmrMain.Interval = 500;
-            this.tmrMain.Tick += new System.EventHandler(this.tmrMain_Tick);
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(711, 576);
-            this.Controls.Add(this.picFlames);
-            this.Controls.Add(this.lstIsland);
-            this.Controls.Add(this.lstMap);
-            this.Controls.Add(this.pic);
-            this.Name = "Form1";
-            this.Text = "Islands Missile Invasion";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
-            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picFlames)).EndInit();
-            this.ResumeLayout(false);
+			this.components = new System.ComponentModel.Container();
+			this.pic = new System.Windows.Forms.PictureBox();
+			this.lstMap = new System.Windows.Forms.ListBox();
+			this.lstIsland = new System.Windows.Forms.ListBox();
+			this.picFlames = new System.Windows.Forms.PictureBox();
+			this.tmrFlames = new System.Windows.Forms.Timer(this.components);
+			this.tmrMain = new System.Windows.Forms.Timer(this.components);
+			((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picFlames)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pic
+			// 
+			this.pic.Location = new System.Drawing.Point(93, 12);
+			this.pic.Name = "pic";
+			this.pic.Size = new System.Drawing.Size(587, 534);
+			this.pic.TabIndex = 0;
+			this.pic.TabStop = false;
+			this.pic.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pic_MouseDown);
+			this.pic.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pic_MouseMove);
+			// 
+			// lstMap
+			// 
+			this.lstMap.Enabled = false;
+			this.lstMap.FormattingEnabled = true;
+			this.lstMap.Location = new System.Drawing.Point(12, 12);
+			this.lstMap.Name = "lstMap";
+			this.lstMap.Size = new System.Drawing.Size(75, 69);
+			this.lstMap.TabIndex = 1;
+			this.lstMap.SelectedIndexChanged += new System.EventHandler(this.lstMap_SelectedIndexChanged);
+			// 
+			// lstIsland
+			// 
+			this.lstIsland.Enabled = false;
+			this.lstIsland.FormattingEnabled = true;
+			this.lstIsland.Location = new System.Drawing.Point(12, 87);
+			this.lstIsland.Name = "lstIsland";
+			this.lstIsland.Size = new System.Drawing.Size(75, 69);
+			this.lstIsland.TabIndex = 2;
+			this.lstIsland.SelectedIndexChanged += new System.EventHandler(this.lstIsland_SelectedIndexChanged);
+			// 
+			// picFlames
+			// 
+			this.picFlames.Location = new System.Drawing.Point(12, 440);
+			this.picFlames.Name = "picFlames";
+			this.picFlames.Size = new System.Drawing.Size(34, 50);
+			this.picFlames.TabIndex = 3;
+			this.picFlames.TabStop = false;
+			// 
+			// tmrFlames
+			// 
+			this.tmrFlames.Tick += new System.EventHandler(this.tmrFlames_Tick);
+			// 
+			// tmrMain
+			// 
+			this.tmrMain.Interval = 500;
+			this.tmrMain.Tick += new System.EventHandler(this.tmrMain_Tick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(702, 576);
+			this.Controls.Add(this.picFlames);
+			this.Controls.Add(this.lstIsland);
+			this.Controls.Add(this.lstMap);
+			this.Controls.Add(this.pic);
+			this.Name = "Form1";
+			this.Text = "Islands Missile Invasion";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+			((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picFlames)).EndInit();
+			this.ResumeLayout(false);
 
         }
 
@@ -112,6 +113,6 @@
         private System.Windows.Forms.PictureBox picFlames;
         private System.Windows.Forms.Timer tmrFlames;
         private System.Windows.Forms.Timer tmrMain;
-    }
+	}
 }
 
--- IslandsApp1/Form1.cs
+++ IslandsApp1/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
@@ -11,285 +12,324 @@ using System.Windows.Forms;
 
 namespace IslandsApp1
 {
-    public partial class Form1 : Form
-    {
-        Bitmap finalImage;
-        bool DoneLoadFlag = false;
-        List<Bitmap> bmpMas = new List<Bitmap>();
-        List<Bitmap> Levels = new List<Bitmap>();
-        List<byte[]> Islands = new List<byte[]>();
-        List<Bitmap> Flames = new List<Bitmap>();
-        List<Bitmap> Explosions = new List<Bitmap>();
-        List<Bitmap> Rectangles = new List<Bitmap>();
-        List<Point> KraterTiles = new List<Point>();
-        Point ExplosionXY = new Point();
-        bool ExplosionFlag = false;
-        Bitmap[] Kursor = new Bitmap[2];
-        Bitmap GreenTile;
-        int KursorX = 0;
-        int KursorY = 0;
-        int flamesCounter = 0;
-        int explosionsCounter = 0;
-        Bitmap[] Krater = new Bitmap[3];
-        List<Point> ValidPlace = new List<Point>();
-        bool ValidCursorFlag = false;
-        int FPS = 30;
-        //#################################################################################
-        private void WriteFile()
-        {
-            int[] mas = new int[10];
-            StreamWriter str = new StreamWriter("temp.txt");
-            for (int i = 0; i < 100; i++)
-            {
-                str.WriteLine(mas[i]);
-            }
-            str.Close();
-        }
+	public partial class Form1 : Form
+	{
+		Bitmap finalImage;
+		bool DoneLoadFlag = false;
+		List<Bitmap> bmpMas = new List<Bitmap>();
+		List<Bitmap> Levels = new List<Bitmap>();
+		List<byte[]> Islands = new List<byte[]>();
+		List<Bitmap> Flames = new List<Bitmap>();
+		List<Bitmap> Explosions = new List<Bitmap>();
+		List<Bitmap> Rectangles = new List<Bitmap>();
+		List<Point> KraterTiles = new List<Point>();
+		Point ExplosionXY = new Point();
+		bool ExplosionFlag = false;
+		Bitmap[] Kursor = new Bitmap[2];
+		Bitmap GreenTile;
+		int KursorX = 0;
+		int KursorY = 0;
+		int flamesCounter = 0;
+		int explosionsCounter = 0;
+		Bitmap[] Krater = new Bitmap[3];
+		List<Point> ValidPlace = new List<Point>();
+		bool ValidCursorFlag = false;
+		int FPS = 30;
+		//#################################################################################
+		private void WriteFile()
+		{
+			int[] mas = new int[10];
+			StreamWriter str = new StreamWriter("temp.txt");
+			for (int i = 0; i < 100; i++)
+			{
+				str.WriteLine(mas[i]);
+			}
+			str.Close();
+		}
 
-        private void RedValidPlaces()
-        {
-            
-            try
-            {
-                string[] separatingStrings = { Environment.NewLine };
+		private void RedValidPlaces()
+		{
 
-
-                string[] words = res.island1d.Split(separatingStrings, StringSplitOptions.RemoveEmptyEntries);
+			try
+			{
+				string[] separatingStrings = { Environment.NewLine };
 
 
-                for (int i = 0; i < words.Count(); i += 2)
-                    ValidPlace.Add(new Point(int.Parse(words[i]), int.Parse(words[i+1])));
-            }
-            catch { }
-        }
-
-        public Form1()
-        {
-            InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            RedValidPlaces();
-
-            pic.Width = 552;
-            pic.Height = 480;
-
-            
-            Kursor[0]= res.rectangles.Clone(new Rectangle(0, 24, 48, 24), PixelFormat.Format16bppArgb1555);
-            Kursor[1] = res.rectangles.Clone(new Rectangle(48, 24, 48, 24), PixelFormat.Format16bppArgb1555);
-
-            Rectangle rect = new Rectangle(48, 0, 48, 24);
-            GreenTile = res.rectangles.Clone(rect, PixelFormat.Format16bppArgb1555);
-
-            Levels.Add(res.tiles1);
-            Levels.Add(res.tiles2);
-            Levels.Add(res.tiles3);
-            
-            Islands.Add(res.island1);
-            Islands.Add(res.island2);
-            Islands.Add(res.island3);
-            Islands.Add(res.island4);
-
-            for (int i = 0; i < 4; i++)
-            {
-                if (i < 3)
-                {
-                    Krater[i]= res.rectangles.Clone(new Rectangle((i+3)*48,24,48,24), PixelFormat.Format16bppArgb1555);
-                    lstMap.Items.Add("Погода " + (i + 1).ToString());
-                }
-                lstIsland.Items.Add("Острів " + (i + 1).ToString());
-            }
-
-            lstMap.SelectedIndex = 0;
-            lstIsland.SelectedIndex = 0;
-
-            DoneLoadFlag = true;
-
-            //MakeLevel(0, 0);
-
-            PrepareFlames(res.flames);
-            PrepareExplosions(res.explosions);
+				string[] words = res.island1d.Split(separatingStrings, StringSplitOptions.RemoveEmptyEntries);
 
 
-            tmrFlames.Enabled = true;
-            tmrMain.Interval = 1000 / 15;
-            tmrMain.Enabled = true;
-        }
+				for (int i = 0; i < words.Count(); i += 2)
+					ValidPlace.Add(new Point(int.Parse(words[i]), int.Parse(words[i + 1])));
+			}
+			catch { }
+		}
 
-        private void PrepareFlames(Bitmap bmp)
-        {
-            Flames.Clear();
-            Rectangle rect;
-            for (int i = 0; i < 4; i++)
-            {
-                rect = new Rectangle(i * 20, 0, 20, 36);
-                Flames.Add(bmp.Clone(rect, PixelFormat.Format16bppArgb1555));
-                GC.Collect();
-            }
-        }
+		public Form1()
+		{
+			InitializeComponent();
+		}
 
-        private void PrepareExplosions(Bitmap bmp)
-        {
-            Explosions.Clear();
-            Rectangle rect;
-            for (int i = 0; i < 6; i++)
-            {
-                Explosions.Add(bmp.Clone(new Rectangle(i * 26, 0, 26, 26), PixelFormat.Format16bppArgb1555));
-            }
-            GC.Collect();
-        }
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			RedValidPlaces();
 
-        private void MakeLevel(int level, int island)
-        {
-            LoadMapImages(Levels[level]);
-            PaintLevel(Islands[island]);
-        }
-
-        private void LoadMapImages(Bitmap bmp)
-        {
-            bmpMas.Clear();
-            Rectangle rect;
-            int n = 0, k = 0;
-            for(int i = 0; i < 34; i++)
-            {
-                rect = new Rectangle(k * 24, n * 24, 24, 24);
-                k++;
-                if(k == 8) { k = 0; n++; }
-                bmpMas.Add(bmp.Clone(rect, PixelFormat.Format16bppArgb1555));
-                GC.Collect();
-            }
-        }
-
-        
-        private void PaintLevel(byte[] LevelMas)
-        {
-            finalImage = new Bitmap(600, 600);
-            using (Graphics g = Graphics.FromImage(finalImage))
-            {
-                //g.RotateTransform(-28,System.Drawing.Drawing2D.MatrixOrder.Prepend);
-                g.Clear(Color.Black);
-                int k = 0, n = 0;
-                for(int i = 0; i < 460; i++)
-                {
-                    g.DrawImage(bmpMas[LevelMas[i]], new Rectangle(k * 24, n * 24, 24, 24));
-                    k++;
-                    if (k == 23) { k = 0; n++; }
-                }
-                foreach(Point p in KraterTiles)
-                    g.DrawImage(Krater[lstMap.SelectedIndex], new Rectangle(p.X, p.Y, 48, 24));
-
-                if (ExplosionFlag)
-                {
-                    g.DrawImage(Explosions[explosionsCounter], new Rectangle(ExplosionXY.X+12, ExplosionXY.Y-12, 24, 24));
-                    explosionsCounter++;
-                    if (explosionsCounter == 6) { explosionsCounter = 0; ExplosionFlag = false; }
-                    GC.Collect();
-                }
-
-                if (ValidCursorFlag)
-                    g.DrawImage(Kursor[0], new Rectangle(KursorX, KursorY, 48, 24));
-                else
-                    g.DrawImage(Kursor[1], new Rectangle(KursorX, KursorY, 48, 24));
+			pic.Width = 552;
+			pic.Height = 480;
 
 
-                g.DrawLine(new Pen(Color.Red, 1), DateTime.Now.Millisecond/30, 0, 60-DateTime.Now.Millisecond/30, 50);
-            }
-            pic.Image = finalImage;
-            GC.Collect();
-        }
+			Kursor[0] = res.rectangles.Clone(new Rectangle(0, 24, 48, 24), PixelFormat.Format16bppArgb1555);
+			Kursor[1] = res.rectangles.Clone(new Rectangle(48, 24, 48, 24), PixelFormat.Format16bppArgb1555);
 
-        private bool IsKursorValid(int x,int y)
-        {
-            Point tmp = new Point(x, y);
-            if (ValidPlace.Contains(tmp))
-                return true;
-            return false; 
-        }
+			Rectangle rect = new Rectangle(48, 0, 48, 24);
+			GreenTile = res.rectangles.Clone(rect, PixelFormat.Format16bppArgb1555);
 
-        private void lstMap_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            if (DoneLoadFlag) MakeLevel(lstMap.SelectedIndex, lstIsland.SelectedIndex);
-        }
+			Levels.Add(res.tiles1);
+			Levels.Add(res.tiles2);
+			Levels.Add(res.tiles3);
 
-        private void lstIsland_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            if(DoneLoadFlag) MakeLevel(lstMap.SelectedIndex, lstIsland.SelectedIndex);
-        }
+			Islands.Add(res.island1);
+			Islands.Add(res.island2);
+			Islands.Add(res.island3);
+			Islands.Add(res.island4);
 
-        private void tmrFlames_Tick(object sender, EventArgs e)
-        {
-            picFlames.Image = null;
-            picFlames.Image = Flames[flamesCounter];
-            flamesCounter++;
-            if (flamesCounter == 4) flamesCounter = 0;
-            GC.Collect();
-        }
+			for (int i = 0; i < 4; i++)
+			{
+				if (i < 3)
+				{
+					Krater[i] = res.rectangles.Clone(new Rectangle((i + 3) * 48, 24, 48, 24), PixelFormat.Format16bppArgb1555);
+					lstMap.Items.Add("Погода " + (i + 1).ToString());
+				}
+				lstIsland.Items.Add("Острів " + (i + 1).ToString());
+			}
 
-        private void RandomMap()
-        {
-            DoneLoadFlag = false;
-            Random rnd = new Random();
-            lstMap.SelectedIndex = rnd.Next(3);
-            lstIsland.SelectedIndex = rnd.Next(4);
-            MakeLevel(lstMap.SelectedIndex, lstIsland.SelectedIndex);
-            DoneLoadFlag = true;
-        }
+			lstMap.SelectedIndex = 0;
+			lstIsland.SelectedIndex = 0;
 
-        private void Form1_KeyUp(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Up)
-            {
-                KursorX += 24;
-                KursorY -= 12;
-            }
-            if (e.KeyCode == Keys.Down)
-            {
-                KursorX -= 24;
-                KursorY += 12;
-            }
-            if (e.KeyCode == Keys.Right)
-            {
-                KursorX += 24;
-                KursorY += 12;
-            }
-            if (e.KeyCode == Keys.Left)
-            {
-                KursorX -= 24;
-                KursorY -= 12;
-            }
-            if (e.KeyCode == Keys.Space)
-            {
-                
-            }
-            if (e.KeyCode == Keys.Enter)
-            {
-                if (e.Modifiers == Keys.Shift)
-                {
-                    if (lstMap.SelectedIndex == lstMap.Items.Count - 1) lstMap.SelectedIndex = 0; else lstMap.SelectedIndex++;
-                }
-                else if (e.Modifiers==Keys.Control)
-                {
-                    if (lstIsland.SelectedIndex == lstIsland.Items.Count - 1) lstIsland.SelectedIndex = 0; else lstIsland.SelectedIndex++;
-                }
-                else if (e.Modifiers == Keys.Alt)
-                {
-                    RandomMap();
-                }
-                else if (!ExplosionFlag)
-                {
-                    ExplosionXY.X = KursorX;
-                    ExplosionXY.Y = KursorY;
-                    ExplosionFlag = true;
-                    KraterTiles.Add(new Point(KursorX, KursorY));
-                }
-            }
-            ValidCursorFlag = IsKursorValid(KursorX, KursorY);
-        }
+			DoneLoadFlag = true;
 
-        private void tmrMain_Tick(object sender, EventArgs e)
-        {
-            MakeLevel(lstMap.SelectedIndex, lstIsland.SelectedIndex);
-        }
-    }
+			//MakeLevel(0, 0);
+
+			PrepareFlames(res.flames);
+			PrepareExplosions(res.explosions);
+
+
+			tmrFlames.Enabled = true;
+			tmrMain.Interval = 1000 / 15;
+			tmrMain.Enabled = true;
+		}
+
+		private void PrepareFlames(Bitmap bmp)
+		{
+			Flames.Clear();
+			Rectangle rect;
+			for (int i = 0; i < 4; i++)
+			{
+				rect = new Rectangle(i * 20, 0, 20, 36);
+				Flames.Add(bmp.Clone(rect, PixelFormat.Format16bppArgb1555));
+				GC.Collect();
+			}
+		}
+
+		private void PrepareExplosions(Bitmap bmp)
+		{
+			Explosions.Clear();
+			Rectangle rect;
+			for (int i = 0; i < 6; i++)
+			{
+				Explosions.Add(bmp.Clone(new Rectangle(i * 26, 0, 26, 26), PixelFormat.Format16bppArgb1555));
+			}
+			GC.Collect();
+		}
+
+		private void MakeLevel(int level, int island)
+		{
+			LoadMapImages(Levels[level]);
+			PaintLevel(Islands[island]);
+		}
+
+		private void LoadMapImages(Bitmap bmp)
+		{
+			bmpMas.Clear();
+			Rectangle rect;
+			int n = 0, k = 0;
+			for (int i = 0; i < 34; i++)
+			{
+				rect = new Rectangle(k * 24, n * 24, 24, 24);
+				k++;
+				if (k == 8) { k = 0; n++; }
+				bmpMas.Add(bmp.Clone(rect, PixelFormat.Format16bppArgb1555));
+				GC.Collect();
+			}
+		}
+
+
+		private void PaintLevel(byte[] LevelMas)
+		{
+			finalImage = new Bitmap(600, 600);
+			using (Graphics g = Graphics.FromImage(finalImage))
+			{
+				//g.RotateTransform(-28,System.Drawing.Drawing2D.MatrixOrder.Prepend);
+				g.Clear(Color.Black);
+				int k = 0, n = 0;
+				for (int i = 0; i < 460; i++)
+				{
+					g.DrawImage(bmpMas[LevelMas[i]], new Rectangle(k * 24, n * 24, 24, 24));
+					k++;
+					if (k == 23) { k = 0; n++; }
+				}
+				foreach (Point p in KraterTiles)
+					g.DrawImage(Krater[lstMap.SelectedIndex], new Rectangle(p.X, p.Y, 48, 24));
+
+				if (ExplosionFlag)
+				{
+					g.DrawImage(Explosions[explosionsCounter], new Rectangle(ExplosionXY.X + 12, ExplosionXY.Y - 12, 24, 24));
+					explosionsCounter++;
+					if (explosionsCounter == 6) { explosionsCounter = 0; ExplosionFlag = false; }
+					GC.Collect();
+				}
+
+				if (ValidCursorFlag)
+					g.DrawImage(Kursor[0], new Rectangle(KursorX, KursorY, 48, 24));
+				else
+					g.DrawImage(Kursor[1], new Rectangle(KursorX, KursorY, 48, 24));
+
+
+				//g.DrawLine(new Pen(Color.Red, 1), DateTime.Now.Millisecond/30, 0, 60-DateTime.Now.Millisecond/30, 50);
+			}
+			pic.Image = finalImage;
+			GC.Collect();
+		}
+
+		private bool IsKursorValid(int x, int y)
+		{
+			Point tmp = new Point(x, y);
+			if (ValidPlace.Contains(tmp))
+				return true;
+			return false;
+		}
+
+		private void lstMap_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if (DoneLoadFlag) MakeLevel(lstMap.SelectedIndex, lstIsland.SelectedIndex);
+		}
+
+		private void lstIsland_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if (DoneLoadFlag) MakeLevel(lstMap.SelectedIndex, lstIsland.SelectedIndex);
+		}
+
+		private void tmrFlames_Tick(object sender, EventArgs e)
+		{
+			picFlames.Image = null;
+			picFlames.Image = Flames[flamesCounter];
+			flamesCounter++;
+			if (flamesCounter == 4) flamesCounter = 0;
+			GC.Collect();
+		}
+
+		private void RandomMap()
+		{
+			DoneLoadFlag = false;
+			Random rnd = new Random();
+			lstMap.SelectedIndex = rnd.Next(3);
+			lstIsland.SelectedIndex = rnd.Next(4);
+			MakeLevel(lstMap.SelectedIndex, lstIsland.SelectedIndex);
+			DoneLoadFlag = true;
+		}
+
+		private void Form1_KeyUp(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Up)
+			{
+				KursorX += 24;
+				KursorY -= 12;
+			}
+			if (e.KeyCode == Keys.Down)
+			{
+				KursorX -= 24;
+				KursorY += 12;
+			}
+			if (e.KeyCode == Keys.Right)
+			{
+				KursorX += 24;
+				KursorY += 12;
+			}
+			if (e.KeyCode == Keys.Left)
+			{
+				KursorX -= 24;
+				KursorY -= 12;
+			}
+			if (e.KeyCode == Keys.Space)
+			{
+
+			}
+			if (e.KeyCode == Keys.Enter)
+			{
+				if (e.Modifiers == Keys.Shift)
+				{
+					if (lstMap.SelectedIndex == lstMap.Items.Count - 1) lstMap.SelectedIndex = 0; else lstMap.SelectedIndex++;
+				}
+				else if (e.Modifiers == Keys.Control)
+				{
+					if (lstIsland.SelectedIndex == lstIsland.Items.Count - 1) lstIsland.SelectedIndex = 0; else lstIsland.SelectedIndex++;
+				}
+				else if (e.Modifiers == Keys.Alt)
+				{
+					RandomMap();
+				}
+				else if (!ExplosionFlag && ValidCursorFlag)
+				{
+					ExplosionXY.X = KursorX;
+					ExplosionXY.Y = KursorY;
+					ExplosionFlag = true;
+					KraterTiles.Add(new Point(KursorX, KursorY));
+				}
+			}
+			ValidCursorFlag = IsKursorValid(KursorX, KursorY);
+		}
+
+		private void tmrMain_Tick(object sender, EventArgs e)
+		{
+			MakeLevel(lstMap.SelectedIndex, lstIsland.SelectedIndex);
+		}
+
+		int tmpX = 0; int tmpY = 0;
+		int dx = 0; int dy = 0;
+		private void pic_MouseMove(object sender, MouseEventArgs e)
+		{
+			dx = e.X % 24;
+			dy = e.Y % 12;
+			//
+			if (dx > 12)
+				tmpX = e.X - dx;
+			else
+				tmpX = e.X - 24 - dx;
+			//
+			if (dy > 6)
+				tmpY = e.Y - dy;
+			else
+				tmpY = e.Y - 12 - dy;
+			//
+			if (IsKursorValid(tmpX, tmpY))
+			{
+				KursorX = tmpX;
+				KursorY = tmpY;
+			}
+			ValidCursorFlag = IsKursorValid(KursorX, KursorY);
+		}
+
+		private void pic_MouseDown(object sender, MouseEventArgs e)
+		{
+			if (ValidCursorFlag)
+			{
+				if (!ExplosionFlag)
+				{
+					ExplosionXY.X = KursorX;
+					ExplosionXY.Y = KursorY;
+					ExplosionFlag = true;
+					KraterTiles.Add(new Point(KursorX, KursorY));
+				}
+			}
+		}
+	}
 }
